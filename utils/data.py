245f7d40540762b0eaa3779c39d343b01de51127
kbogdan <karina.bogdan@gmail.com>
2016-11-06 17:41:02 -0200

    Defining input tensor and input of ingredients

--- utils/data.py
+++ utils/data.py
@@ -4,6 +4,10 @@ import numpy as np
 import math
 import random
 
+from keras.preprocessing.image import load_img, img_to_array
+from keras.applications import vgg16
+from keras import backend as K
+
 
 def recipes_ids(json_file):
     """Get the ids of all recipes in a list.
@@ -79,16 +83,66 @@ def split_data(json_file, data_dir, images_dir='images',
         return train_path, test_path, data_train, data_test
 
 
-def load(dir):
-    """ Load images from dir (directory)"""
+def ingredients_vector(recipe_ingredients, ingredients_list,
+                       random=False, nb_ingredients=100, size=1):
+    """ Returns a vector of size len(ingredients_list) that indicate the presence or not of each
+        ingredient of ingredients_list in recipe_ingredients.
+        recipe_ingredients: ingredients of the recipe
+        ingredients_list: all ingredients used to create our vector (all possible ingredients considering all the
+        dataset)
+        random: if True returns a random ingredient vector of size N"""
+    if random:
+        return np.random.random_integers(low=0, high=1, size=(size, nb_ingredients))
+    else:
+        # TODO quando terminar pre-processamento ingredients
+        our_ing_vector = np.zeros(len(ingredients_list), dtype=np.uint8)
 
-    return 1
+        return our_ing_vector
 
+
+# Util function to open, resize and format pictures into appropriate tensors
+# (from an example in the keras documentation)
+def preprocess_image(image_path, img_height=224, img_width=224):
+    img = load_img(image_path, target_size=(img_height, img_width))  # target_size=(img_nrows, img_ncols)
+    img = img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+    img = vgg16.preprocess_input(img)
+    return img
+
+
+def load(data, dir_images, img_height, img_width):
+    """ Load images from dir (directory) and ingredients of all recipes in data.
+        Return a tensor combining all tensor for each image, and a numpy list of ingredients"""
+    list_images = []
+    list_of_all_ingredients = []  # TODO carregar quando terminar pre-processamento ingredientes
+
+    # input_ingredients = np.zeros((len(data), len(list_of_all_ingredients)), dtype=np.uint8)  correto depois dos TODOs
+    input_ingredients = np.zeros((len(data), 100), dtype=np.uint8)
+
+    index = 0
+    for id_recipe in data:
+        # Get tensor representations of our images
+        image = data[id_recipe]['file_image']
+        current_image = K.variable(preprocess_image(dir_images + image, img_height, img_width))
+        list_images.append(current_image)
+
+        # Get ingredients_input
+        list_ingredients = data[id_recipe]['ingredients']
+        input_ingredients[index, :] = ingredients_vector(list_ingredients, list_of_all_ingredients, random=True)  #TODO
+
+        index += 1
+
+    # combine the 3 images into a single Keras tensor
+    # input_tensor = K.concatenate([base_image, style_reference_image], axis=0)
+    input_tensor = K.concatenate(list_images, axis=0)
+
+    return input_tensor, input_ingredients
 
 
 def main():
-    split_data('recipes-ctc.json', '../data/recipes-ctc/', train=0.8)
+    train_path, test_path, data_train, data_test = split_data('recipes-ctc.json', '../data/recipes-ctc/', train=0.8)
 
+    load(data_train, train_path, 224, 224)
 
 if __name__ == '__main__':
     main()
