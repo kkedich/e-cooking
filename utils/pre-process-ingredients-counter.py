f66a390dae3552c340d9cd83946202274979218b
suelen <suelen.apsilva@gmail.com>
2016-11-08 00:15:33 -0200

    Ingredients words counter - code improvements

--- utils/pre-process-ingredients-counter.py
+++ utils/pre-process-ingredients-counter.py
@@ -8,55 +8,138 @@ from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import CountVectorizer
 from collections import Counter
 from collections import OrderedDict
+import sys
+
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+def cleanhtml(raw_html):
+  """ Remove html tags from a text """
+  cleanr = re.compile('<.*?>')
+  cleantext = re.sub(cleanr, '', raw_html)
+
+  return cleantext
+
+def clean_recipes_terms(ingredient):
+  """ Remove common terms in ingredients of recipes. """
+  ingredient = re.sub("\d+", "", ingredient)
+  ingredient = re.sub("/", "", ingredient)
+  ingredient = re.sub(",", "", ingredient)
+  ingredient = re.sub("ml", "", ingredient)
+  ingredient = re.sub("cups", "", ingredient)
+  ingredient = re.sub("cup", "", ingredient)
+  ingredient = re.sub("tablespoons", "", ingredient)
+  ingredient = re.sub("tablespoon", "", ingredient)
+  ingredient = re.sub("teaspoons", "", ingredient)
+  ingredient = re.sub("teaspoon", "", ingredient)
+  ingredient = re.sub("spoons", "", ingredient)
+  ingredient = re.sub("spoon", "", ingredient)
+  ingredient = re.sub("finely", "", ingredient)
+  ingredient = re.sub("pounded", "", ingredient)
+  ingredient = re.sub("pound", "", ingredient)
+  ingredient = re.sub("diced", "", ingredient)
+  ingredient = re.sub("minced", "", ingredient)
+  ingredient = re.sub("freshly", "", ingredient)
+  ingredient = re.sub("fresh", "", ingredient)
+  ingredient = re.sub("handful", "", ingredient)
+  ingredient = re.sub("chopped", "", ingredient)
+  ingredient = re.sub("canned", "", ingredient)
+  ingredient = re.sub("soft", "", ingredient)
+  ingredient = re.sub("large", "", ingredient)
+  ingredient = re.sub("bunch", "", ingredient)
+  ingredient = re.sub("cut", "", ingredient)
+  ingredient = re.sub("ounces", "", ingredient)
+  ingredient = re.sub("ounce", "", ingredient)
+  ingredient = re.sub("crumbled", "", ingredient)
+  ingredient = re.sub("chilled", "", ingredient)
+  ingredient = re.sub("sliced", "", ingredient)
+  ingredient = re.sub("wedges", "", ingredient)
+  ingredient = re.sub("wedge", "", ingredient)
+  ingredient = re.sub("small", "", ingredient)
+  ingredient = re.sub("generous", "", ingredient)
+  ingredient = re.sub("finely", "", ingredient)
+  ingredient = re.sub("stems", "", ingredient)
+  ingredient = re.sub("removed", "", ingredient)
+  ingredient = re.sub("serving", "", ingredient)
+  ingredient = re.sub("spreading", "", ingredient)
+  ingredient = re.sub("cubed", "", ingredient)
+  ingredient = re.sub("to taste", "", ingredient)
+  ingredient = re.sub("unsalted", "", ingredient)
+  ingredient = re.sub("grated", "", ingredient)
+  ingredient = re.sub("boneless", "", ingredient)
+  ingredient = re.sub("skinless", "", ingredient)
+  ingredient = re.sub("inch", "", ingredient)
+  ingredient = re.sub("thick", "", ingredient)
+  ingredient = re.sub("ground", "", ingredient)
+  ingredient = re.sub("crushed", "", ingredient)
+  ingredient = re.sub("package", "", ingredient)
+  ingredient = re.sub("pack", "", ingredient)
+  ingredient = re.sub("scraped", "", ingredient)
+  ingredient = re.sub("medium", "", ingredient)
+  ingredient = re.sub("all-purpose", "", ingredient)
+  ingredient = re.sub("heads", "", ingredient)
+  ingredient = re.sub("head", "", ingredient)
+
+  return ingredient
 
 
-file_names = []
+def read_cached_ingredients_words():
+    """ Returns a list with all words from all ingredients from all recipes of dataset """
 
-# get a list of all files
-file_names = glob.glob("images/*.*")
+    config = json.loads(open('pre-processed-full-recipes-dataset.json').read())
 
-# check if the base was already indexed
-if not os.path.exists('recipes.txt'):
-    with open("recipes.txt", 'w') as f:
-        for s in file_names:
-            s = s.split("--")[1]
-            s = s.split(".")[0]
-            f.write(s + '\n')
+    words = ""
+    ingr_word_list = []
 
-with open("recipes.txt", 'r') as f:
-    recipes_code = [line.rstrip('\n') for line in f]
+    if not os.path.exists('words.txt'):
+        with open("words.txt", 'w') as f:
+            
+            for recipe in config.keys():
 
-config = json.loads(open('pre-processed-recipes-ctc.json').read())
+                for ingredient in config[recipe]["ingredients"]:
+                    ingredient = cleanhtml(ingredient) 
+                    ingredient = clean_recipes_terms(ingredient)
+                    word_list = ingredient.split()
+                    for w in word_list:
+                        f.write(w + '\n')
+                    
+    with open("words.txt", 'r') as f:
+        ingr_word_list = [line.rstrip('\n') for line in f]
 
-words = ""
-ingr_word_list = []
+    return ingr_word_list
 
-if not os.path.exists('words.txt'):
-    with open("words.txt", 'w') as f:
-        
-        for recipe in recipes_code:
 
-            print(config[recipe]["name"])
 
-            for ingredient in config[recipe]["ingredients"]:
-                ingredient = myutils.cleanhtml(ingredient) 
-                
-                ingredient = myutils.clean_recipes_terms(ingredient)
-                word_list = ingredient.split()
-                for w in word_list:
-                    f.write(w + '\n')
-                
-with open("words.txt", 'r') as f:
-    ingr_word_list = [line.rstrip('\n') for line in f]
+def cache_counts(counts):
+    """ Saves the values to a file for human analysis """
 
-stemmer = SnowballStemmer("english")
-vectorizer = CountVectorizer(stop_words="english")
+    with open("ingredients.txt", 'w') as f:
+        for more_freq in counts.keys():
+            if counts[more_freq] > 750:
+                f.write(more_freq+';'+str(counts[more_freq])+'\n')
 
-filtered_words = [word for word in ingr_word_list if word not in stopwords.words('english')]
-filtered_words[:] = [stemmer.stem(word) for word in filtered_words]
-counts = Counter(filtered_words)
 
-with open("ingredients.txt", 'w') as f:
-    for more_freq in counts.keys():
-        if counts[more_freq] > 150:
-            f.write(more_freq+';'+str(counts[more_freq])+'\n')
+def count_words_of_ingredients_list():
+    """ Counts the number of ocurrencies of each word in dataset, after removing the most
+    common stopwords and performing stemming """
+
+    stemmer = SnowballStemmer("english")
+    vectorizer = CountVectorizer(stop_words="english")
+
+    ingr_word_list = read_cached_ingredients_words()
+
+    filtered_words = [word for word in ingr_word_list if word not in stopwords.words('english')]
+    filtered_words[:] = [stemmer.stem(word) for word in filtered_words]
+    counts = Counter(filtered_words)
+
+    cache_counts(counts)
+
+    return counts
+
+
+def main():
+    count_words_of_ingredients_list()
+    
+
+if __name__ == '__main__':
+    main()
